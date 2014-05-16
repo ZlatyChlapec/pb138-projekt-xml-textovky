19aaf9ee990da0ae512a655b5992a477dfdbe567
blake2556@gmail.com <blake2556@gmail.com@2b8e6b15-5208-1c8d-c6d3-a876a87dccdd>
2014-05-16 00:21:33 +0000

    first GUI complete

--- trunk/src/main/java/cz/muni/fi/pb138/TextGame.java
+++ trunk/src/main/java/cz/muni/fi/pb138/TextGame.java
@@ -1,9 +1,19 @@
 package cz.muni.fi.pb138;
 
+import java.awt.*;
+
 /**
- * @author Slapy
+ * @author Martin Zaťko
  * @version 15.5.2014
  */
 public class TextGame {
-    //just test if it is finally working
+    public static void main(String...args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GUI frame = new GUI();
+                frame.setVisible(true);
+            }
+        });
+    }
 }
