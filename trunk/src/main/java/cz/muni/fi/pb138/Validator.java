843976a9e7e58cf95299467af955ac2dd3a99402
stefan.malcek1@gmail.com <stefan.malcek1@gmail.com@2b8e6b15-5208-1c8d-c6d3-a876a87dccdd>
2014-06-03 16:19:57 +0000

    Validator - tests

--- trunk/src/main/java/cz/muni/fi/pb138/Validator.java
+++ trunk/src/main/java/cz/muni/fi/pb138/Validator.java
@@ -4,11 +4,18 @@
  */
 package cz.muni.fi.pb138;
 
+import java.io.IOException;
+
 /**
  *
  * @author Filip Sonta
  * @version 18.5.2014
  */
 public class Validator {
-    
+    public Validator(String schema){
+    }
+
+    public String validate(String xmlName)throws IOException{
+        return null;
+    }
 }
