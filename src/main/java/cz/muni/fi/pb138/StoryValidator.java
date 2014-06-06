3467c28b305a0286f49e956110b38002ed43297a
khazrozaw <khazrozaw@gmail.com>
2014-06-06 16:39:35 +0000

    GameScene class, StoryValidator class created (empty), SceneParser created (not working - fixing in progress), fixed XMLSchema (choices can be empty - final scene)

---
+++ src/main/java/cz/muni/fi/pb138/StoryValidator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138;
+
+import org.w3c.dom.Document;
+
+/**
+ *
+ * @author Filip
+ */
+public class StoryValidator {
+    private Document doc;
+    
+    public void validateGameStory(String xmlName) {
+        
+    }
+}
--- src/main/java/cz/muni/fi/pb138/TextGame.java
+++ src/main/java/cz/muni/fi/pb138/TextGame.java
@@ -1,6 +1,12 @@
 package cz.muni.fi.pb138;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  * @author Martin Zaťko
@@ -8,14 +14,27 @@ import java.awt.*;
  */
 public class TextGame {
     public static void main(String...args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                GUI frame = new GUI();
-                frame.setVisible(true);
-            }
-        });
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                GUI frame = new GUI();
+//                frame.setVisible(true);
+//            }
+//        });
+//        
+        File oo = new File("src/test/resources/testXML.xml");
+        try {
+            SceneParser test = SceneParser.newInstance(oo);
+        } catch (SAXException ex) {
+            Logger.getLogger(TextGame.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(TextGame.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TextGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        
+        GameScene sc;
+        sc = SceneParser.parseScene(1);
+//        System.out.println(sc.getSceneName());
     }
 }
