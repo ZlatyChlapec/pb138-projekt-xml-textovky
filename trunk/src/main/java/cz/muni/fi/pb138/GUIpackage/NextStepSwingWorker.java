d01bb3a09ce7c84495803df0dd6b3c89526514fd
blake2556@gmail.com <blake2556@gmail.com@2b8e6b15-5208-1c8d-c6d3-a876a87dccdd>
2014-06-13 12:39:38 +0000

    bit changed logic in GUI

--- trunk/src/main/java/cz/muni/fi/pb138/GUIpackage/NextStepSwingWorker.java
+++ trunk/src/main/java/cz/muni/fi/pb138/GUIpackage/NextStepSwingWorker.java
@@ -1,45 +1,39 @@
 package cz.muni.fi.pb138.GUIpackage;
 
 import cz.muni.fi.pb138.Choice;
-import cz.muni.fi.pb138.GUIpackage.LoadStorySwingWorker;
 import cz.muni.fi.pb138.GameScene;
 import cz.muni.fi.pb138.TextGame;
+
 import javax.swing.*;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Provide information for next step.
+ *
  * @author Martin Zaťko
  * @version 10.6.2014
  */
 public class NextStepSwingWorker extends SwingWorker<Void, Void> {
 
     private JFrame mainFrame;
-    private JLabel sceneNameLabel;
-    private JLabel actualSceneLabel;
-    private JButton firstOptionButton;
-    private JButton secondOptionButton;
-    private JButton thirdOptionButton;
-    private JButton fourthOptionButton;
     private Choice[] choices;
     private int choice;
     private String button;
     private GameScene scene;
 
-    public NextStepSwingWorker(JFrame mainFrame, JLabel sceneNameLabel, JLabel actualSceneLabel,
-                               JButton firstOptionButton, JButton secondOptionButton, JButton thirdOptionButton,
-                               JButton fourthOptionButton, String button) {
+    public NextStepSwingWorker(JFrame mainFrame, String button) {
         this.mainFrame = mainFrame;
-        this.sceneNameLabel = sceneNameLabel;
-        this.actualSceneLabel = actualSceneLabel;
-        this.firstOptionButton = firstOptionButton;
-        this.secondOptionButton = secondOptionButton;
-        this.thirdOptionButton = thirdOptionButton;
-        this.fourthOptionButton = fourthOptionButton;
         this.button = button;
     }
 
+    /**
+     * Actualize information about scenes.
+     *
+     * @return Void
+     * @throws Exception If something happen throw it.
+     */
     @Override
     protected Void doInBackground() throws Exception {
         switch (button) {
@@ -59,91 +53,27 @@ public class NextStepSwingWorker extends SwingWorker<Void, Void> {
                 scene = null;
                 break;
         }
-        choice = scene.getChoicesCount();
-        choices = new Choice[choice];
-        for (int i = 0; i < choice; i++) {
-            choices[i] = scene.getChoice(i);
+        if (scene != null) {
+            choice = scene.getChoicesCount();
+            choices = new Choice[choice];
+            for (int i = 0; i < choice; i++) {
+                choices[i] = scene.getChoice(i);
+            }
+            TextGame.choices = choices;
+            TextGame.gameScene = scene;
         }
         return null;
     }
 
+    /**
+     * If something went wrong let us know.
+     */
     @Override
     protected void done() {
         try {
             get();
             if (scene == null) {
                 JOptionPane.showMessageDialog(mainFrame, "There are some serious errors in code.", "FATAL ERROR", JOptionPane.ERROR_MESSAGE);
-            }
-            sceneNameLabel.setText(scene.getSceneName());
-            actualSceneLabel.setText("<html>"+ scene.getSceneDesc() +"</html>");
-            switch (choice) {
-                case 0:
-                    firstOptionButton.setEnabled(false);
-                    firstOptionButton.setText("");
-                    secondOptionButton.setEnabled(false);
-                    secondOptionButton.setText("");
-                    thirdOptionButton.setEnabled(false);
-                    thirdOptionButton.setText("");
-                    fourthOptionButton.setEnabled(false);
-                    fourthOptionButton.setText("");
-                    JOptionPane.showMessageDialog(mainFrame, "Congratulations you made it.", "Game over", JOptionPane.INFORMATION_MESSAGE);
-                    break;
-                case 1:
-                    firstOptionButton.setText("<html>"+ choices[0].getText() +"</html>");
-                    firstOptionButton.setEnabled(true);
-                    secondOptionButton.setText("");
-                    secondOptionButton.setEnabled(false);
-                    thirdOptionButton.setText("");
-                    thirdOptionButton.setEnabled(false);
-                    fourthOptionButton.setText("");
-                    fourthOptionButton.setEnabled(false);
-                    TextGame.firstOption = choices[0].getGoTo();
-                    TextGame.secondOption = -1;
-                    TextGame.thirdOption = -1;
-                    TextGame.fourthOption = -1;
-                    break;
-                case 2:
-                    firstOptionButton.setText("<html>"+ choices[0].getText() +"</html>");
-                    firstOptionButton.setEnabled(true);
-                    secondOptionButton.setText("<html>" + choices[1].getText() + "</html>");
-                    secondOptionButton.setEnabled(true);
-                    thirdOptionButton.setText("");
-                    thirdOptionButton.setEnabled(false);
-                    fourthOptionButton.setText("");
-                    fourthOptionButton.setEnabled(false);
-                    TextGame.firstOption = choices[0].getGoTo();
-                    TextGame.secondOption = choices[1].getGoTo();
-                    TextGame.thirdOption = -1;
-                    TextGame.fourthOption = -1;
-                    break;
-                case 3:
-                    firstOptionButton.setText("<html>"+ choices[0].getText() +"</html>");
-                    firstOptionButton.setEnabled(true);
-                    secondOptionButton.setText("<html>" + choices[1].getText() + "</html>");
-                    secondOptionButton.setEnabled(true);
-                    thirdOptionButton.setText("<html>" + choices[2].getText() + "</html>");
-                    thirdOptionButton.setEnabled(true);
-                    fourthOptionButton.setText("");
-                    fourthOptionButton.setEnabled(false);
-                    TextGame.firstOption = choices[0].getGoTo();
-                    TextGame.secondOption = choices[1].getGoTo();
-                    TextGame.thirdOption = choices[2].getGoTo();
-                    TextGame.fourthOption = -1;
-                    break;
-                case 4:
-                    firstOptionButton.setText("<html>"+ choices[0].getText() +"</html>");
-                    firstOptionButton.setEnabled(true);
-                    secondOptionButton.setText("<html>"+ choices[1].getText() +"</html>");
-                    secondOptionButton.setEnabled(true);
-                    thirdOptionButton.setText("<html>"+ choices[2].getText() +"</html>");
-                    thirdOptionButton.setEnabled(true);
-                    fourthOptionButton.setText("<html>"+ choices[3].getText() +"</html>");
-                    fourthOptionButton.setEnabled(true);
-                    TextGame.firstOption = choices[0].getGoTo();
-                    TextGame.secondOption = choices[1].getGoTo();
-                    TextGame.thirdOption = choices[2].getGoTo();
-                    TextGame.fourthOption = choices[3].getGoTo();
-                    break;
             }
         } catch (InterruptedException e) {
             JOptionPane.showMessageDialog(mainFrame, "Something interrupted process while loading story. Check logger for further information.", "Failed to load story.", JOptionPane.ERROR_MESSAGE);
