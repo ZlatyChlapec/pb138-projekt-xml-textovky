73eaaebde53647c5c40551f9a6ce2d3c6e2882ac
blake2556@gmail.com <blake2556@gmail.com@2b8e6b15-5208-1c8d-c6d3-a876a87dccdd>
2014-06-15 10:00:41 +0000

    added feature recent games need a little debug

--- trunk/src/main/java/cz/muni/fi/pb138/TextGame.java
+++ trunk/src/main/java/cz/muni/fi/pb138/TextGame.java
@@ -1,10 +1,18 @@
 package cz.muni.fi.pb138;
 
 import cz.muni.fi.pb138.GUIpackage.GUI;
+import cz.muni.fi.pb138.GUIpackage.LoadStorySwingWorker;
 
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main class which with you should run program.
@@ -18,12 +26,30 @@ public class TextGame {
     public static Choice[] choices;
     public static String gameName;
     public static GameScene gameScene;
+    public static String firstRecent;
+    public static String secondRecent;
+    public static String thirdRecent;
+    public static String fourthRecent;
     public static long firstOption = -1;
     public static long secondOption = -1;
     public static long thirdOption = -1;
     public static long fourthOption = -1;
 
     public static void main(String...args) {
+
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            firstRecent = prop.getProperty("recentlyUsed0");
+            secondRecent = prop.getProperty("recentlyUsed1");
+            thirdRecent = prop.getProperty("recentlyUsed2");
+            fourthRecent = prop.getProperty("recentlyUsed3");
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(LoadStorySwingWorker.class.getName()).log(Level.SEVERE, null, e);
+        } catch (IOException e) {
+            Logger.getLogger(LoadStorySwingWorker.class.getName()).log(Level.SEVERE, null, e);
+        }
+
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
